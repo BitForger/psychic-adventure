accb8c032ba0050690ea406ea4262f1bc159bb46
Noah Kovacs <kovacsn66@gmail.com>
2020-12-20 03:30:37 -0500

    chore: latest work

---
+++ src/views/Chat.vue
@@ -0,0 +1,34 @@
+<template>
+  <code>
+    {{this.result}}
+  </code>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator';
+
+@Component({
+  name: 'Chat',
+  async mounted () {
+  },
+  computed: {
+  },
+  data: {
+  }
+})
+export default class Chat extends Vue {
+  private result: any;
+
+  constructor () {
+    super();
+    this.axios.get('http://localhost:3000/keys/keypair').then(value => {
+      this.result = JSON.stringify(value.data, null, 4);
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
